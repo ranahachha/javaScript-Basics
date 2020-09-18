b46ae279560faa2c7972745153e4b0b594c47d70
Neekhil Rai <linkinrai@gmail.com>
2020-09-18 16:22:40 +0200

    examples spread and rest operator

--- fundamental.js
+++ fundamental.js
@@ -36,3 +36,59 @@ console.log(`${brand} was established in ${est}. The beer alcohol volume is ${al
 
 const{brand : make, est : estd, alc: alcohol} = beer;
 console.log(`${make} was established in ${estd}. The beer alcohol volume is ${alcohol}%.`)
+
+/* spread: spread operator takes an array or any iterables and spreads its value across
+Eg without spread operator to concatinate two arrays. */
+
+const communicable = [
+    "Zika",
+    "Ebola",
+    "Mrsa",
+    "hiv/aids"
+]
+const noncommnicable = [
+    "cancer",
+    "diabetes",
+    "Asthma"
+]
+
+const disease = communicable.concat(noncommnicable);
+disease.forEach(disease => console.log(disease));
+
+//with spread perator
+
+const allDisease = [
+    ...communicable,
+    "cancer",
+    "diabetes",
+    "Asthma"
+]
+
+console.log("-----------------------------------")
+allDisease.forEach(disease => console.log(disease));
+
+/* spread operator can be used as function argument
+Eg. without spread operator */
+
+function add(a, b, c){
+    console.log(a+b+c);
+}
+
+const args = [10, 20, 30];
+add.apply(null, args);
+
+//eg. with spread operator
+
+add(...args);
+
+/* rest operator: allows to represent indefinite number of arguments as an Array
+rest operator collects multiple elements and "condenses" them into a single element */
+
+function mul(...num){
+    let total = 1;
+    for(const i of num){//for..of iterates over value for..in iterates over properties
+        total = total * i;
+    }
+    return total;
+}
+console.log(mul(2, 3, 4))
